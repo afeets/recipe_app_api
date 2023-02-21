8c53df3b0ebbe9806e9d5cfee72abb021d70c106
Andy Feetenby <andyfeetenby@gmail.com>
2023-02-21 16:15:00 +0700

    Added Custom fields to Django Admin GUI for adding and changing users

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,47 @@
-from django.contrib import admin # noqa
+"""
+Django Admin Customization
+"""
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext as _
+from core import models
 
-# Register your models here.
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users"""
+    ordering = ['id']
+    list_display = ['email', 'name']
+
+    """Support fields in custom admin model"""
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (_('Important Dates'), {'fields': ('last_login',)}),
+    )
+
+    readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser'
+            )
+        }),
+    )
+
+"""Force it use UserAdmin"""
+admin.site.register(models.User, UserAdmin)
