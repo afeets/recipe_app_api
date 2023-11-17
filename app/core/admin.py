34df8549ec21abdf90283f37036b925db2d5667d
Andy Feetenby <andyfeetenby@gmail.com>
2023-11-17 10:15:46 +0700

    Added Tag model

--- app/core/admin.py
+++ app/core/admin.py
@@ -9,42 +9,47 @@ from core import models
 
 class UserAdmin(BaseUserAdmin):
     """Define the admin pages for users"""
-    ordering = ['id']
-    list_display = ['email', 'name']
+
+    ordering = ["id"]
+    list_display = ["email", "name"]
 
     """Support fields in custom admin model"""
     fieldsets = (
-        (None, {'fields': ('email', 'password')}),
+        (None, {"fields": ("email", "password")}),
         (
-            _('Permissions'),
+            _("Permissions"),
             {
-                'fields': (
-                    'is_active',
-                    'is_staff',
-                    'is_superuser',
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
                 )
-            }
+            },
         ),
-        (_('Important Dates'), {'fields': ('last_login',)}),
+        (_("Important Dates"), {"fields": ("last_login",)}),
     )
 
-    readonly_fields = ['last_login']
+    readonly_fields = ["last_login"]
     add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': (
-                'email',
-                'password1',
-                'password2',
-                'name',
-                'is_active',
-                'is_staff',
-                'is_superuser'
-            )
-        }),
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                    "name",
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                ),
+            },
+        ),
     )
 
 
 """Force it use UserAdmin"""
 admin.site.register(models.User, UserAdmin)
 admin.site.register(models.Recipe)
+admin.site.register(models.Tag)
