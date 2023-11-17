be13a4ce8615acfdca5c0744202073bedb206e44
Andy Feetenby <andyfeetenby@gmail.com>
2023-11-17 14:02:30 +0700

    Added function to delete Tag via API

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -84,3 +84,15 @@ class PrivateTagsApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         tag.refresh_from_db()
         self.assertEqual(tag.name, payload['name'])
+
+    def test_delete_tag(self):
+        """ Test deleting a tag """
+        tag = Tag.objects.create(user=self.user, name='Tag to delete')
+
+        url = detail_url(tag.id)
+        res = self.client.delete(url)
+
+        """ Now confirm no tags exists """
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+        tags = Tag.objects.filter(user=self.user)
+        self.assertFalse(tags.exists())
